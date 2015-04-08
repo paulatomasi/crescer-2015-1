c601bd900dc5e8a9312c238286ca8a55f1f80de5
Paula Tomasi <paulatomasi@outlook.com>
2015-04-08 17:03:20 -0300

    add(ContadorDeNumeros)

---
+++ src/ContadorDeNumeros.java
@@ -0,0 +1,19 @@
+/**
+ * Cria objetos contador de numeros
+ * 
+ * @author Paula
+ */
+public class ContadorDeNumeros
+{
+    public void contarAté(int numero){
+        for (int x = 1; x<=numero; x++){
+            System.out.println(x);
+        }
+    }
+    
+    public void contarRegressivamente(int numero){
+        for (int x = numero; x>=1; x--){
+            System.out.println(x);
+        }
+    }
+}
