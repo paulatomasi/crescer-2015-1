3ada048638dc9a1224ac3059412ad7dbb86440a0
unknown <projetocrescer@207V-20.acad.feevale.br>
2015-04-13 14:31:18 -0300

    feat(ElfoVerde) Alteração do método atirarFlecha().

--- src/ElfoVerde.java
+++ src/ElfoVerde.java
@@ -9,17 +9,17 @@ public class ElfoVerde extends Elfo
     public ElfoVerde(String nome){
         super(nome);
     }
+    
     public ElfoVerde(String nome, int flechas){
         this(nome);
         super.flechas = flechas;
     }
-
+    
     protected void atirarFlecha(Orc orc) {
-        flechas--;
-        experiencia+=2;
-        orc.recebeAtaque();
+        super.atirarFlecha(orc);
+        this.experiencia++;
     }
-
+    
     public void adicionarItem(ItemDoInventario item){
         boolean descricaoDoItem = item.getDescricao().equalsIgnoreCase("Espada de aco valiriano") || item.getDescricao().equalsIgnoreCase("Arco e Flecha de vidro");
         if (descricaoDoItem){
