80806b75f2e8bc917d64750beca2b24e4ffd1c8e
Paula Tomasi de Andrade <paulatomasi@outlook.com>
2015-05-03 20:05:39 -0300

    feat(EstrategiaNoturna)

--- src/EstrategiaNoturna.java
+++ src/EstrategiaNoturna.java
@@ -1,24 +1,33 @@
 import java.util.*;
-/**
- * @author Paula Tomasi
- */
 public class EstrategiaNoturna implements EstrategiaDeAtaque
 {
+    private ArrayList<Elfo> ordemAtaque = new ArrayList<>();
+    
     public void atacarOrcs(ArrayList<Elfo> elfos, ArrayList<Orc> orcs) {
         System.out.println("Super estratégia noturna.. ATIVAR!!!");
-        int quantidadeDeAtaques = elfos.size() * orcs.size();
-        int limiteElfosNoturnos = (int)(quantidadeDeAtaques * 0.3);
-        int quantidadeDeElfosNoturnosQueJáAtacaram = 0;
+        
+        int qtdAtaques = elfos.size() * orcs.size();
+        int limiteElfosNoturnos = (int)(qtdAtaques * 0.3);
+        int qtdElfosNoturnosQueJáAtacaram = 0;
+        
         for (Elfo elfoQueVaiAtacar : elfos) {
-            boolean eElfoNoturno = elfoQueVaiAtacar instanceof ElfoNoturno;
-            if (eElfoNoturno) {
-                if (quantidadeDeElfosNoturnosQueJáAtacaram >= limiteElfosNoturnos)
+            boolean éElfoNoturno = elfoQueVaiAtacar instanceof ElfoNoturno;
+            
+            if (éElfoNoturno) {
+                if (qtdElfosNoturnosQueJáAtacaram >= limiteElfosNoturnos)
                     continue;
-                quantidadeDeElfosNoturnosQueJáAtacaram++;
+                qtdElfosNoturnosQueJáAtacaram++;
+                ordemAtaque.add(elfoQueVaiAtacar);
             }
+            
             for (Orc orc : orcs) {
                 elfoQueVaiAtacar.atirarFlecha(orc);
             }
+            
         }
+    }
+    
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return ordemAtaque;
     }
 }
