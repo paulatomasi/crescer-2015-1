22bd532e3f1f95fdd4546539f13c885207df090d
Paula Tomasi de Andrade <paulatomasi@outlook.com>
2015-05-03 20:05:20 -0300

    feat(EstrategiaNormal)

--- src/EstrategiaNormal.java
+++ src/EstrategiaNormal.java
@@ -4,12 +4,19 @@ import java.util.*;
  */
 
 public class EstrategiaNormal implements EstrategiaDeAtaque {
+    private ArrayList<Elfo> ordemAtaque = new ArrayList<>();
+    
     public void atacarOrcs(ArrayList<Elfo> elfos, ArrayList<Orc> orcs) {
         System.out.println("Estratégia normalzinha...");
         for (Elfo elfo : elfos) {
+            ordemAtaque.add(elfo);
             for (Orc orc : orcs) {
                 elfo.atirarFlecha(orc);
             }
         }        
+    }
+    
+     public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return ordemAtaque;
     }
 }
