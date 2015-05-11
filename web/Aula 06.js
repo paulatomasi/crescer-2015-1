9dd0202d619efa4da4f1e79e34e2c2ad97f49b87
Paula Tomasi <paulatomasi@outlook.com>
2015-05-11 17:03:04 -0300

    JavaScript, aula 6

---
+++ web/Aula 06.js
@@ -0,0 +1,12 @@
+'use scrict'; // restringe gambiarras
+
+var oi = "oi";
+
+String.prototype.toBacon = function(){
+	return this + " BACON!!!"; // this se refere a variavel que vai ser utilizada
+	}
+	
+oi.toBacon();
+
+var a = 10;
+console.log(a);
