7cda8402ddff171d95cfc0d6cb9769a1f466e3d6
Arionildo <arionildo6@gmail.com>
2015-05-29 15:13:25 -0300

    feat(filmator-web)

--- java/filmator-web/src/main/java/filmator/controller/FilmeController.java
+++ java/filmator-web/src/main/java/filmator/controller/FilmeController.java
@@ -1,12 +1,9 @@
 package filmator.controller;
 
 import javax.inject.Inject;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import filmator.dao.FilmeDao;
 import filmator.model.Filme;
 
