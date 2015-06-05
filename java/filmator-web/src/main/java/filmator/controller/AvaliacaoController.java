13de87d266dc8baf7de2342d6fd048f5815ef164
Paula Tomasi <paulatomasi@outlook.com>
2015-06-04 23:26:21 -0300

    feat(filmator-web) Adição de ajax

--- java/filmator-web/src/main/java/filmator/controller/AvaliacaoController.java
+++ java/filmator-web/src/main/java/filmator/controller/AvaliacaoController.java
@@ -11,22 +11,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import filmator.dao.AvaliacaoDao;
 import filmator.model.Avaliacao;
 
-	@Controller
-	public class AvaliacaoController {
-		@Inject
-		AvaliacaoDao avaliacoes;
-	
-		@RequestMapping(value = "/salvarAvaliacao", method = RequestMethod.POST)
-		public String salvarAvaliacao(Model model, HttpSession session, Avaliacao avaliacao){
-			String login = (String) session.getAttribute("usuarioLogado");
+@Controller
+public class AvaliacaoController {
+	@Inject
+	AvaliacaoDao avaliacoes;
+
+	@RequestMapping(value = "/salvaravaliacao", method = RequestMethod.POST)
+	public String salvarAvaliacao(Avaliacao avaliacao, Model model, HttpSession session){
+		String login = (String) session.getAttribute("usuarioLogado");
+		if(login!=null){
 			int idFilme = avaliacao.getIdFilme();
 			int nota = avaliacao.getNota();
-			if (avaliacoes.getNota(avaliacao.getIdFilme(), login).isEmpty()){
+			if (avaliacoes.getNota(idFilme, login).isEmpty()){
 				avaliacoes.salvarAvaliacao(login, idFilme, nota);
 			}else{
 				avaliacoes.alterarAvalicao(login, idFilme, nota);
-			}	
-			return "redirect:/home";
+			}
+		}
+		return "redirect:/home";
 	}
 
 }
